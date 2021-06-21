c153f9fb0292d7a8d3c921baec80fa396cc2fbab
aliciamorillo <63121998+aliciamorillo@users.noreply.github.com>
2021-06-21 10:51:20 +0200

    Add files via upload

---
+++ part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+public class AverageOfPositiveNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = 0;
+        int elements = 0;
+        
+        while(true){
+            int input = Integer.valueOf(scanner.nextLine());
+            
+            if(input == 0){
+                break;
+            }
+            
+            if(input > 0){
+                count += input;
+                elements++;
+            } 
+            
+        }
+        
+        if(count == 0){
+            System.out.println("Cannot calculate the average");
+        } else {
+            System.out.println(1.0*count / elements);
+        }
+        
+    }
+    
+}
